b3b5d33a01129a4f7f0176d5f08ff0c8ebdb246e
cyborg-moonbase <70490537+cyborg-moonbase@users.noreply.github.com>
2023-04-27 17:08:10 -0500

    Create payload.py

---
+++ payload.py
@@ -0,0 +1,24 @@
+"""
+Creates an injectable payload from an executable binary.
+"""
+
+import builtins
+from sys import argv
+from base64 import b64encode
+
+# input should be a windows executable
+input_binary = argv[1]
+# output can be a .txt or file with any suffix
+output_payload = argv[2]
+
+def make_payload(input_bin, output_file):
+    with open(input_bin, mode='rb') as f:
+        x = f.read()
+        byte_array = bytearray(x)
+        encoded_payload = b64encode(byte_array)
+        with open(output_file, mode='w', encoding='utf-8') as g:
+            g.write(encoded_payload.decode('utf-8'))
+            
+            
+if __name__ == '__main__':
+    make_payload(input_binary, output_payload)
